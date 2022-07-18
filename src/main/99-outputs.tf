02273bcf1fbfb471f1b27c1866ce488117b89e06
uolter <walter.traspadini@pagopa.it>
2022-07-18 14:29:05 +0200

    aws ses service for pagopa.gov.it

---
+++ src/main/99-outputs.tf
@@ -0,0 +1,16 @@
+output "verification_token" {
+  value = module.ses.verification_token
+}
+
+output "dkim_tokens" {
+  value = module.ses.dkim_tokens
+}
+
+output "ses_user_access_key_id" {
+  value = module.ses.ses_user_access_key_id
+}
+
+output "ses_user_secret_access_key" {
+  value     = module.ses.ses_user_secret_access_key
+  sensitive = true
+}
