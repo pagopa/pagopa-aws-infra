53811a8a49001a9c94e4ef72c4184812d59e09dc
uolter <walter.traspadini@pagopa.it>
2022-08-10 16:05:01 +0200

    removed invalid actions

--- src/main/10-ses.tf
+++ src/main/10-ses.tf
@@ -4,7 +4,10 @@ module "ses" {
   aws_region = var.aws_region
 
   iam_permissions = [
-    "ses:SendEmail"
+    "ses:SendEmail",
+    "ses:GetSendQuota",
+    "ses:SendRawEmail",
+    "ses:SendTemplatedEmail"
   ]
 
   ses_group_name = "pagoPaSES"
