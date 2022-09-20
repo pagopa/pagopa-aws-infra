216743d10bf7fe5c17fc7d6fc1dda70246dc5967
Walter Traspadini <walter.traspadini@pagopa.it>
2022-09-20 14:57:05 +0200

    Sending quota alerts (#8)
    * sns topic to send alerts
    
    * alert sending quota
    
    * new alarm to observe the reputation complaint rate
    
    * update ses module version with alarms

--- src/main/10-ses.tf
+++ src/main/10-ses.tf
@@ -1,8 +1,8 @@
 module "ses" {
-  source     = "github.com/pagopa/terraform-aws-ses.git?ref=v1.1.0"
-  domain     = "pagopa.gov.it"
+  source              = "github.com/pagopa/terraform-aws-ses.git?ref=v1.2.0"
+  domain              = "pagopa.gov.it"
   mail_from_subdomain = "email"
-  aws_region = var.aws_region
+  aws_region          = var.aws_region
 
   iam_permissions = [
     "ses:SendCustomVerificationEmail",
@@ -23,5 +23,17 @@ module "ses" {
       resources = ["*"]
     }
   ]
+
+  alarms = {
+    actions                    = [aws_sns_topic.alarms.arn]
+    daily_send_quota_threshold = 40000
+    daily_send_quota_period    = 60 * 60 * 24 # 1 day
+
+    reputation_complaint_rate_threshold = 0.8
+    reputation_complaint_rate_period    = 60 * 60 # 1 hour.
+
+    reputation_bounce_rate_threshold = 0.1
+    reputation_bounce_rate_period    = 5 * 60 # 5min
+  }
 
 }
