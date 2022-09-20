9f94a87295e7099ca71ab95267640527d9f07a8d
uolter <walter.traspadini@pagopa.it>
2022-09-20 14:46:23 +0200

    domain identiy for pagopa.it

--- src/main/10-ses.tf
+++ src/main/10-ses.tf
@@ -1,8 +1,8 @@
 module "ses" {
-  source     = "github.com/pagopa/terraform-aws-ses.git?ref=v1.1.0"
-  domain     = "pagopa.gov.it"
+  source              = "github.com/pagopa/terraform-aws-ses.git?ref=v1.1.0"
+  domain              = "pagopa.gov.it"
   mail_from_subdomain = "email"
-  aws_region = var.aws_region
+  aws_region          = var.aws_region
 
   iam_permissions = [
     "ses:SendCustomVerificationEmail",
@@ -23,5 +23,32 @@ module "ses" {
       resources = ["*"]
     }
   ]
+}
 
+
+module "ses" {
+  source              = "github.com/pagopa/terraform-aws-ses.git?ref=v1.1.0"
+  domain              = "pagopa.it"
+  mail_from_subdomain = "email"
+  aws_region          = var.aws_region
+
+  iam_permissions = [
+    "ses:SendCustomVerificationEmail",
+    "ses:SendEmail",
+    "ses:SendRawEmail",
+    "ses:SendTemplatedEmail"
+  ]
+
+  ses_group_name = "NotificationService"
+  user_name      = "notification-service"
+
+  iam_allowed_resources = [format("arn:aws:ses:%s:%s:identity/*", var.aws_region, data.aws_caller_identity.current.id)]
+
+  iam_additional_statements = [
+    {
+      sid       = "Statistics"
+      actions   = ["ses:GetSendQuota"]
+      resources = ["*"]
+    }
+  ]
 }
